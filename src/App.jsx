d76ca5a59f474bed52d71bfb4501f57216fa4379
sebokdas1 <sebok.das.ff@gmail.com>
2023-06-01 11:01:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Try from "./componants/Try";
+
+function App() {
+  return (
+    <>
+      <Try />
+    </>
+  );
+}
+
+export default App;
---
+++ src/componants/Try.jsx
@@ -0,0 +1,10 @@
+import "./Try.css";
+const Try = () => {
+  return (
+    <div>
+      <h1 className="myTitle">My name is sebok das</h1>
+    </div>
+  );
+};
+
+export default Try;
